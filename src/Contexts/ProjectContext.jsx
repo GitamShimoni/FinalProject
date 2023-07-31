b9fabe5dcf43a07e1444af5d822c2221735dd83e
pazkopo01~ <pazkopo@gmail.com>
2023-07-31 10:14:08 +0300

    add contractor form

--- src/Contexts/ProjectContext.jsx
+++ src/Contexts/ProjectContext.jsx
@@ -2,9 +2,10 @@ import { createContext, useState } from "react";
 export const ProjectContext = createContext();
 export const ProjectProvider = ({ children }) => {
   const [tools, setTools] = useState([]);
+  const [contracors, setContractors] = useState([]);
   const [projectId, setProjectId] = useState("");
   return (
-    <ProjectContext.Provider value={{ tools, setTools }}>
+    <ProjectContext.Provider value={{ tools, setTools, contracors, setContractors }}>
       {children}
     </ProjectContext.Provider>
   );
